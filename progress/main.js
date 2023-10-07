af268e57d7fca598d26829c83152be95490cebb9
ebliscode <ebliscode@gmail.com>
2023-10-06 21:52:30 -0400

    chore: add progress status to progress bar

--- progress/main.js
+++ progress/main.js
@@ -1,11 +1,15 @@
 const progressBar = document.getElementById("progress-bar");
 const progressBarValue = document.getElementById("progress-bar-value");
+const content = document.getElementById("progress-bar-content");
 
 const ancho = document.getElementById("ancho");
 const alto = document.getElementById("alto");
 const border_radius = document.getElementById("border-radius");
 const progress = document.getElementById("progress");
 const color = document.getElementById("color");
+
+if (parseInt(progress.value) > 50) content.style.color = "#fff";
+else content.style.color = "#000";
 
 ancho.addEventListener("change", () => {
   progressBar.style.width = ancho.value + "px";
@@ -22,8 +26,13 @@ border_radius.addEventListener("change", () => {
 
 progress.addEventListener("change", () => {
   progressBarValue.style.width = progress.value + "%";
+  content.innerHTML = progress.value + " %";
+  if (parseInt(progress.value) > 50) content.style.color = "#fff";
+  else content.style.color = "#000";
 });
 
 color.addEventListener("change", () => {
   progressBarValue.style.backgroundColor = color.value;
+  if (parseInt(progress.value) > 50) content.style.color = "#fff";
+  else content.style.color = "#000";
 });
